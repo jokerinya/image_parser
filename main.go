4a9c30f77a79c44fec440e187082657e2855fd3e
Ibrahim Sakaci <ibrahim.sakaci@svea.com>
2025-05-01 14:31:39 +0300

    move async image to own file

--- main.go
+++ main.go
@@ -2,76 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/jokerinya/image_parser/helpers"
+	"time"
 )
 
 func main() {
 	imagePaths := []string{"image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"}
-	fmt.Printf("Images to parse: %d\n", len(imagePaths))
-	imagesChan := loadImages(imagePaths)
-	resizedChan := resizeImages(imagesChan)
-	grayScaledChan := grayScaleImages(resizedChan)
-	savedImagesNum := saveImages(grayScaledChan)
-	fmt.Printf("Saved images number: %d\n", savedImagesNum)
-}
-
-func loadImages(imagePaths []string) <-chan helpers.Img {
-	out := make(chan helpers.Img)
-	go func() {
-		for _, path := range imagePaths {
-			img, err := helpers.LoadImage(path)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			data := &helpers.Img{
-				Filename: path,
-				Data:     img,
-			}
-			out <- *data
-		}
-		close(out)
-	}()
-	return out
-}
-
-func resizeImages(in <-chan helpers.Img) <-chan helpers.Img {
-	out := make(chan helpers.Img)
-	go func() {
-		for data := range in {
-			data.Resize()
-			out <- data
-		}
-		close(out)
-	}()
-	return out
-}
-
-func grayScaleImages(in <-chan helpers.Img) <-chan helpers.Img {
-	out := make(chan helpers.Img)
-	go func() {
-		for data := range in {
-			data.GrayScale()
-			out <- data
-		}
-		close(out)
-	}()
-	return out
-}
-
-func saveImages(in <-chan helpers.Img) int {
-	resultChan := make(chan int)
-	go func() {
-		count := 0
-		for data := range in {
-			if err := data.SaveToFile(); err != nil {
-				fmt.Println(err)
-				continue
-			}
-			count++
-		}
-		resultChan <- count
-		close(resultChan)
-	}()
-	return <-resultChan
+	start := time.Now()
+	pipelineImageParsing(imagePaths)
+	elapsed := time.Since(start)
+	fmt.Println("async elapsed: ", elapsed)
 }
---
+++ pipelineImageParsing.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jokerinya/image_parser/helpers"
+)
+
+func pipelineImageParsing(imagePaths []string) {
+	fmt.Printf("Images to parse: %d\n", len(imagePaths))
+	imagesChan := loadImages(imagePaths)
+	resizedChan := resizeImages(imagesChan)
+	grayScaledChan := grayScaleImages(resizedChan)
+	savedImagesNum := saveImages(grayScaledChan)
+	fmt.Printf("Saved images number: %d\n", savedImagesNum)
+}
+
+func loadImages(imagePaths []string) <-chan helpers.Img {
+	out := make(chan helpers.Img)
+	go func() {
+		for _, path := range imagePaths {
+			img, err := helpers.LoadImage(path)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			data := &helpers.Img{
+				Filename: path,
+				Data:     img,
+			}
+			out <- *data
+		}
+		close(out)
+	}()
+	return out
+}
+
+func resizeImages(in <-chan helpers.Img) <-chan helpers.Img {
+	out := make(chan helpers.Img)
+	go func() {
+		for data := range in {
+			data.Resize()
+			out <- data
+		}
+		close(out)
+	}()
+	return out
+}
+
+func grayScaleImages(in <-chan helpers.Img) <-chan helpers.Img {
+	out := make(chan helpers.Img)
+	go func() {
+		for data := range in {
+			data.GrayScale()
+			out <- data
+		}
+		close(out)
+	}()
+	return out
+}
+
+func saveImages(in <-chan helpers.Img) int {
+	resultChan := make(chan int)
+	go func() {
+		count := 0
+		for data := range in {
+			if err := data.SaveToFile(); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			count++
+		}
+		resultChan <- count
+		close(resultChan)
+	}()
+	return <-resultChan
+}
